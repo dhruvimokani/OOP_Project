7ff0e2e37b22b48fe39b0c397c2d6cc9bca4dcca
Dhruvi Mokani <dhruvimokani08@gmail.com>
2026-04-27 21:26:31 +0530

    registration and login form

--- project.cpp
+++ project.cpp
@@ -1,200 +1,165 @@
-#include<iostream>
-#include<fstream>
-#include<windows.h>
-#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <windows.h>
 using namespace std;
 
-class Login //login class
+class Login
 {
-private:     string LoginID,Password;
-public:
-    void setID(string id){ LoginID=id; }
-    void setPW(string pw){ Password=pw; }
+private:
+    string LoginID, Password;
 
-    string getID(){ return LoginID; }
-    string getPW(){ return Password; }
+public:
+    void setID(string id) 
+    { 
+        LoginID = id; 
+    }
+    void setPW(string pw) 
+    {
+         Password = pw; 
+    }
+
+    string getID()
+     {
+         return LoginID;
+     }
+    string getPW()
+     {
+         return Password;
+     }
 };
 
-bool isUserExists(string id)   //if user already exist with same id
+// check user exists
+bool isUserExists(string id)
 {
     ifstream infile("Login.txt");
     string line;
-    while(getline(infile,line))
+
+    while (getline(infile, line))
     {
         stringstream ss(line);
         string uid, pw;
+
         getline(ss, uid, ':');
         getline(ss, pw);
 
-        if(uid == id)
+        if (uid == id)
             return true;
     }
     return false;
 }
 
-void changePassword(string id)      // Change password 
-{
-    ifstream infile("Login.txt");
-    ofstream temp("temp.txt");
-    string line;
-    string newpw;
-
-    cout<<"\tEnter New Password (min 8 chars): ";
-    cin>>newpw;
-    if(newpw.length() < 8)
-    {
-        cout<<"\tPassword too short!\n";
-        return;
-    }
-    while(getline(infile,line))
-    {
-        stringstream ss(line);
-        string uid, pw;
-        getline(ss, uid, ':');
-        getline(ss, pw);
-
-        if(uid == id)
-        {
-            temp << uid << ":" << newpw << endl;
-        }
-        else
-        {
-            temp << uid << ":" << pw << endl;
-        }
-    }
-
-    infile.close();
-    temp.close();
-    remove("Login.txt");
-    rename("temp.txt","Login.txt");
-
-    cout<<"\tPassword Changed Successfully!\n";
-}
-
-void deleteAccount(string id)               //Delete account
-{
-    ifstream infile("Login.txt");
-    ofstream temp("temp.txt");
-    string line;
-
-    while(getline(infile,line))
-    {
-        stringstream ss(line);
-        string uid, pw;
-        getline(ss, uid, ':');
-        getline(ss, pw);
-        if(uid != id)
-        {
-            temp << uid << ":" << pw << endl;
-        }
-    }
-
-    infile.close();
-    temp.close();
-    remove("Login.txt");
-    rename("temp.txt","Login.txt");
-    cout<<"\tAccount Deleted Successfully!\n";
-}
-
-void userMenu(string id)
-{
-    int choice;
-    do {
-        cout<<"\n\n\t===== USER MENU =====\n";
-        cout<<"\t1. Change Password\n";
-        cout<<"\t2. Delete Account\n";
-        cout<<"\t3. Logout\n";
-        cout<<"\tEnter choice: ";
-        cin>>choice;
-        if(choice == 1)
-        {
-            changePassword(id);
-        }
-        else if(choice == 2)
-        {
-            deleteAccount(id);
-            break;
-        }
-    } while(choice != 3);
-}
-
+// register
 void registration()
 {
     system("cls");
-    string id,pw;
-    cout<<"\tEnter Login ID: ";
-    cin>>id;
-    if(isUserExists(id))
+
+    string id, pw;
+    cout << "\tEnter Login ID: ";
+    cin >> id;
+
+    if (isUserExists(id))
     {
-        cout<<"\tUser already exists!\n";
+        cout << "\tUser already exists!\n";
         Sleep(1500);
         return;
     }
-    do {
-        cout<<"\tEnter Password (min 8 chars): ";
-        cin>>pw;
-        if(pw.length()<8)
-            cout<<"\tToo short!\n";
-    } while(pw.length()<8);
+
+    do
+    {
+        cout << "\tEnter Password (min 8 chars): ";
+        cin >> pw;
+
+        if (pw.length() < 8)
+            cout << "\tToo short!\n";
+
+    } while (pw.length() < 8);
 
     ofstream outfile("Login.txt", ios::app);
     outfile << id << ":" << pw << endl;
-    cout<<"\tRegistered Successfully!\n";
+
+    cout << "\tRegistered Successfully!\n";
     Sleep(1000);
 }
 
+// after login
+void afterLogin(string id)
+{
+    cout << "\nWelcome " << id << " !!\n";
+}
+
+// login
 void login()
 {
     system("cls");
-    string id,pw;
-    cout<<"\tEnter Login ID: ";
-    cin>>id;
-    cout<<"\tEnter Password: ";
-    cin>>pw;
+
+    string id, pw;
+    cout << "\tEnter Login ID: ";
+    cin >> id;
+
+    cout << "\tEnter Password: ";
+    cin >> pw;
 
     ifstream infile("Login.txt");
     string line;
-    bool found=false;
-    while(getline(infile,line))
+    bool found = false;
+
+    while (getline(infile, line))
     {
         stringstream ss(line);
         string uid, upw;
+
         getline(ss, uid, ':');
         getline(ss, upw);
 
-        if(id==uid && pw==upw)
+        if (id == uid && pw == upw)
         {
-            found=true;
-            cout<<"\tLogin Successful...\n";
-            Sleep(1000);
-            userMenu(id);
+            found = true;
+            cout << "\tLogin Successful...\n";
+            afterLogin(id);
+            Sleep(3000);
             break;
         }
     }
-    if(!found)
-        cout<<"\tInvalid ID or Password!\n";
+
+    if (!found)
+        cout << "\tInvalid ID or Password!\n";
+
     infile.close();
 }
 
 int main()
 {
     int choice;
-    while(true)
+
+    while (true)
     {
         system("cls");
-        cout<<"\n\t===== LOGIN SYSTEM =====\n";
-        cout<<"\t1. Register\n";
-        cout<<"\t2. Login\n";
-        cout<<"\t3. Exit\n";
-        cout<<"\tEnter choice: ";
-        cin>>choice;
 
-        if(choice==1)
+        cout << "\n\t===== LOGIN SYSTEM =====\n";
+        cout << "\t1. Register\n";
+        cout << "\t2. Login\n";
+        cout << "\t3. Exit\n";
+        cout << "\tEnter choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
             registration();
-        else if(choice==2)
-            login();
-        else if(choice==3)
             break;
-        Sleep(500);
+
+        case 2:
+            login();
+            break;
+
+        case 3:
+            return 0;
+
+        default:
+            cout << "\tInvalid choice!\n";
+            Sleep(1000);
+        }
     }
 }
